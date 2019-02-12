65a0c6c562c0612afcb258e8b2b5e4327d41175d
jose antonio izquierdo <jizquierdo@osnids.com>
2019-02-13 00:39:46 +0100

    -- main.go owlh version and other stuff --

--- routers/router.go
+++ routers/router.go
@@ -1,8 +1,8 @@
-// @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
-// @TermsOfServiceUrl http://beego.me/
+// @APIVersion 0.1.0
+// @Title OwlH Node API
+// @Description OwlH node API
+// @Contact support@owlh.net
+// @TermsOfServiceUrl http://www.owlh.net
 // @License Apache 2.0
 // @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
