87f51596b3a0054da9e4ca21c3bf0fc6bc468139
jose antonio izquierdo <jizquierdo@osnids.com>
2019-02-12 17:29:19 +0100

    -- routers clean & rm older files --

--- routers/router.go
+++ routers/router.go
@@ -9,22 +9,11 @@ package routers
 
 import (
 	"owlhnode/controllers"
-
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
 		beego.NSNamespace("/suricata",
 			beego.NSInclude(
 				&controllers.SuricataController{},
