8599806ee0d9abfc8b55ac3ad230b757a8702adc
jose antonio izquierdo <jizquierdo@osnids.com>
2019-08-15 13:44:53 +0200

    geolocation

--- geolocation/geolocation.go
+++ geolocation/geolocation.go
@@ -12,23 +12,9 @@ func Init() {
         logs.Error(err)
     }
     defer db.Close()
-    // If you are using strings that may be invalid, check that ip is not nil
-    ip := net.ParseIP("137.101.151.36")
-    record, err := db.City(ip)
-    if err != nil {
-        logs.Error(err)
-    }
-    logs.Info("Portuguese (BR) city name: %v\n", record.City.Names["en"])
-    logs.Info("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
-    logs.Info("Russian country name: %v\n", record.Country.Names["en"])
-    logs.Info("ISO country code: %v\n", record.Country.IsoCode)
-    logs.Info("Time zone: %v\n", record.Location.TimeZone)
-    logs.Info("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
 }
 
-
-
-func GetGeoInfo(ip string)(geoinfo map[string]string{}) {
+func GetGeoInfo(ip string)(geoinfo map[string]string) {
     ip := net.ParseIP(ip)
     record, err := db.City(ip)
     if err != nil {
