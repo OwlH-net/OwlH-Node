79ea8a8c567b417761cca4ade0c2d0b46d664cd6
jose antonio izquierdo <jizquierdo@osnids.com>
2019-08-15 10:22:51 +0200

    mapper - seek info

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -191,7 +191,7 @@ func Starttail(file string) {
     var seekv tail.SeekInfo
     seekv.Offset = 0
     seekv.Whence = os.SEEK_END
-    t, _ := tail.TailFile(file, tail.Config{Follow: true, Location: seekv})
+    t, _ := tail.TailFile(file, tail.Config{Follow: true, Location: *seekv})
     for line := range t.Lines {
         dispatch(line.Text)
     }
