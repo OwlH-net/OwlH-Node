29c91db2d552397e5afc578587a4346e9bf8c298
jose antonio izquierdo <jizquierdo@osnids.com>
2019-08-15 13:42:48 +0200

    geolocation -

--- geolocation/geolocation.go
+++ geolocation/geolocation.go
@@ -28,16 +28,17 @@ func Init() {
 
 
 
-func GetGeoInfo(ip string)geoinfo string {
+func GetGeoInfo(ip string)(geoinfo map[string]string{}) {
     ip := net.ParseIP(ip)
     record, err := db.City(ip)
     if err != nil {
         logs.Error(err)
     }
-    geoinfo = map[string]string{}
+    //geoinfo = map[string]string{}
     geoinfo["city_name"] = record.City.Names["en"]
     geoinfo["county"] = record.Subdivisions[0].Names["en"]
     geoinfo["country"] = record.Country.Names["en"]
     geoinfo["country_code"] = record.Country.IsoCode
     geoinfo["continent_code"] = record.Continent.Code
+    return geoinfo
 }
