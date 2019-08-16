510532153a04debd89b3898077348a63cdc1fd4e
jose antonio izquierdo <jizquierdo@osnids.com>
2019-08-16 11:49:46 +0200

    monitor - mem and disk

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -5,12 +5,18 @@ import (
     "runtime"
     "time"
     "github.com/pbnjay/memory"
+    "github.com/shirou/gopsutil/cpu"
+    "github.com/shirou/gopsutil/disk"
+    "github.com/shirou/gopsutil/host"
+    "github.com/shirou/gopsutil/mem"
+    "github.com/shirou/gopsutil/net"
 )
 
 func monitor() {
     for {
         time.Sleep(time.Second * 20)
         PrintMemUsage()
+        PrintDiskUsage()
     }
 }
 
@@ -21,10 +27,15 @@ func Init() {
 
 
 func PrintMemUsage() {
-        var m runtime.MemStats
-        runtime.ReadMemStats(&m)
-        logs.Info("Monitor -> Mem Stats")
-        logs.Notice("Alloc = %v MiB\tTotalAlloc = %v MiB\tSys = %v MiB\tNumGC = %v\tTotal system memory: %d", bToMb(m.Alloc),bToMb(m.TotalAlloc),bToMb(m.Sys), m.NumGC,bToMb(memory.TotalMemory()))
+    var m runtime.MemStats
+    runtime.ReadMemStats(&m)
+    logs.Info("Monitor -> Mem Stats")
+    logs.Notice("Alloc = %v MiB\tTotalAlloc = %v MiB\tSys = %v MiB\tNumGC = %v\tTotal system memory: %d MiB", bToMb(m.Alloc),bToMb(m.TotalAlloc),bToMb(m.Sys), m.NumGC,bToMb(memory.TotalMemory()))
+}
+
+func PrintDiskUsage() {
+    diskStat, _ := disk.Usage("/")
+    logs.Notice("Total Disk: %s\t Used Disk: %s\t Free Disk: %s", strconv.FormatUint(diskStat.Total, 10), strconv.FormatUint(diskStat.Used, 10), strconv.FormatUint(diskStat.Free, 10)) 
 }
 
 func bToMb(b uint64) uint64 {
