f2fa97fbe94a6006df3dfc64c16f03aca023e941
jose antonio izquierdo <jizquierdo@osnids.com>
2019-08-16 10:06:34 +0200

    Monitr - memcheck

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,16 +1,34 @@
 package monitor
 
 import (
-
+    "github.com/astaxie/beego/logs"
+    "runtime"
+    "time"
 )
 
 func monitor() {
     for {
-        
+        time.Sleep(time.Second * 20)
+        PrintMemUsage()
     }
 }
 
 func Init() {
     logs.Info("Monitor -> Starting Monitor Service")
     go monitor()
+}
+
+
+func PrintMemUsage() {
+        var m runtime.MemStats
+        runtime.ReadMemStats(&m)
+        logs.Info("Monitor -> Mem Stats")
+        logs.Notice("Alloc = %v MiB", bToMb(m.Alloc))
+        logs.Notice("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+        logs.Notice("\tSys = %v MiB", bToMb(m.Sys))
+        logs.Notice("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+    return b / 1024 / 1024
 }
