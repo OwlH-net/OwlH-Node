e797d3dee20172fcc847e20b001bac1a3fb0a6a5
sortizowlh <sortiz@owlh.net>
2019-12-05 18:19:32 +0100

    Suricata and STAP now shows status and running for node and master

--- zeek/zeek.go
+++ zeek/zeek.go
@@ -142,7 +142,10 @@ func ZeekStatus() (zeekstatus []ZeekNode, err error) {
         logs.Error(newError)
         // return nil, newError
     }
-    logs.Info(string(output))
+    logs.Debug(string(output))
+    logs.Debug(string(output))
+    logs.Debug(string(output))
+    logs.Debug(string(output))
     nodes := []ZeekNode{}
     outputlines := strings.Split(string(output),"\n")
     for outputline := range outputlines {
