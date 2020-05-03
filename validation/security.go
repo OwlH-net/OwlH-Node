13ec51b0a160a399568578804d644ffdd1811923
jose antonio izquierdo <jizquierdo@owlh.net>
2020-05-03 18:53:56 +0200

    untar

--- validation/security.go
+++ validation/security.go
@@ -5,64 +5,81 @@ import (
 	// jwt "github.com/dgrijalva/jwt-go"
 	// "golang.org/x/crypto/bcrypt"
 	// "errors"
+	"owlhnode/database"
 	"strings"
-    "owlhnode/database"
-    // "owlhmaster/utils"
+	// "owlhmaster/utils"
 )
 
 func UserPermissionsValidation(uuidUser string, permissionRequest string) (val bool, err error) {
-	allRelations, err := ndb.GetUserGroupRoles(); if err != nil {logs.Error("UserPermissionsValidation error getting permissions: %s",err); return false, err}
-	rolePerm, err := ndb.GetRolePermissions(); if err != nil {logs.Error("UserPermissionsValidation error getting user rolePermissions: %s",err); return false, err}
-	allPerm, err := ndb.GetPermissions(); if err != nil {logs.Error("UserPermissionsValidation error getting user GetPermissions: %s",err); return false, err}
-	
+	allRelations, err := ndb.GetUserGroupRoles()
+	if err != nil {
+		logs.Error("UserPermissionsValidation error getting permissions: %s", err)
+		return false, err
+	}
+	rolePerm, err := ndb.GetRolePermissions()
+	if err != nil {
+		logs.Error("UserPermissionsValidation error getting user rolePermissions: %s", err)
+		return false, err
+	}
+	allPerm, err := ndb.GetPermissions()
+	if err != nil {
+		logs.Error("UserPermissionsValidation error getting user GetPermissions: %s", err)
+		return false, err
+	}
+
 	//check if permission exists
 	permExists := false
-	for x := range allPerm{
-		if x == permissionRequest {permExists = true}
+	for x := range allPerm {
+		if x == permissionRequest {
+			permExists = true
+		}
 	}
-	if !permExists {logs.Error("Permissions validation error - This permission don't exists"); return false,err}
+	if !permExists {
+		logs.Error("Permissions validation error - This permission don't exists")
+		return false, err
+	}
 
-	for x := range allRelations{
-		if allRelations[x]["user"] == uuidUser{
+	for x := range allRelations {
+		if allRelations[x]["user"] == uuidUser {
 			//Check if user role has admin permissions
-			for w := range rolePerm{
-				if allRelations[x]["role"] == rolePerm[w]["role"] && allRelations[x]["role"] != ""{
+			for w := range rolePerm {
+				if allRelations[x]["role"] == rolePerm[w]["role"] && allRelations[x]["role"] != "" {
 					//split permissions
 					permissionsList := strings.Split(rolePerm[w]["permissions"], ",")
 					//check if user is admin
-					for p := range permissionsList{
+					for p := range permissionsList {
 						if permissionsList[p] == "admin" {
 							return true, nil
-						}					
-					}					
+						}
+					}
 					//check for permissions requested
-					for s := range permissionsList{
+					for s := range permissionsList {
 						if permissionsList[s] == permissionRequest {
 							return true, nil
-						}					
-					}					
+						}
+					}
 				}
 			}
 
 			//check group roles for user groups
-			for s := range allRelations{
-				if allRelations[s]["group"] == allRelations[x]["group"] && allRelations[x]["group"] != ""{
-					for roleID := range rolePerm{
-						if rolePerm[roleID]["role"] == allRelations[s]["role"]{
+			for s := range allRelations {
+				if allRelations[s]["group"] == allRelations[x]["group"] && allRelations[x]["group"] != "" {
+					for roleID := range rolePerm {
+						if rolePerm[roleID]["role"] == allRelations[s]["role"] {
 							//split permissions
 							permissionsList := strings.Split(rolePerm[roleID]["permissions"], ",")
 							//check if user is admin
-							for p := range permissionsList{
+							for p := range permissionsList {
 								if permissionsList[p] == "admin" {
 									return true, nil
-								}					
-							}					
+								}
+							}
 							//check for permissions requested
-							for s := range permissionsList{
+							for s := range permissionsList {
 								if permissionsList[s] == permissionRequest {
 									return true, nil
-								}					
-							}	
+								}
+							}
 						}
 					}
 				}
--- validation/validation.go
+++ validation/validation.go
@@ -1,23 +1,26 @@
 package validation
 
 import (
+	"errors"
 	"github.com/astaxie/beego/logs"
 	jwt "github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
-	"errors"
-    "owlhnode/database"
+	"owlhnode/database"
 )
 
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    logs.Info("NEW HASH PASSWD--> "+string(bytes))
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	logs.Info("NEW HASH PASSWD--> " + string(bytes))
+	return string(bytes), err
 }
 
 func CheckPasswordHash(password string, hash string) (bool, error) {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))	
-	if err != nil {logs.Error("validation/CheckPasswordHash "+err.Error()); return false, err}
-    return true, nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		logs.Error("validation/CheckPasswordHash " + err.Error())
+		return false, err
+	}
+	return true, nil
 }
 
 // Generates a jwt token.
@@ -26,40 +29,52 @@ func Encode(secret string) (val string, err error) {
 		ExpiresAt: 15000,
 		Issuer:    "OwlH",
 	}
-	
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString([]byte(secret))
-	if err != nil {logs.Error("Encode error: %s", err); return "", err}
+	if err != nil {
+		logs.Error("Encode error: %s", err)
+		return "", err
+	}
 	return tokenString, err
 }
 
-func VerifyToken(token string, userUuid string)(err error){
-	masters,err := ndb.GetMasters(); if err != nil {logs.Error("CheckToken error getting master data: %s", err); return err}
-	for x := range masters{
+func VerifyToken(token string, userUuid string) (err error) {
+	masters, err := ndb.GetMasters()
+	if err != nil {
+		logs.Error("CheckToken error getting master data: %s", err)
+		return err
+	}
+	for x := range masters {
 		tkn, err := Encode(masters[x]["secret"])
 		if err != nil {
-			logs.Error("Error checking Master token: %s", err); return err
-		}else{
+			logs.Error("Error checking Master token: %s", err)
+			return err
+		} else {
 			if token == tkn {
-			// 	if userUuid == "none"{
-					return nil
-			// 	}else{
-			// 		return errors.New("This user has not enough privileges level")
-			// 	}
-			// }else{
-			// 	return errors.New("The token retrieved is false")
+				//     if userUuid == "none"{
+				return nil
+				//     }else{
+				//         return errors.New("This user has not enough privileges level")
+				//     }
+				// }else{
+				//     return errors.New("The token retrieved is false")
 			}
-		}		
+		}
 	}
 	return errors.New("VerifyToken - Incorrect Token")
 }
 
-func VerifyPermissions(uuidUser string, object string, permissions []string)(hasPermissions bool, err error){
-	for x := range permissions{
-		status,err := UserPermissionsValidation(uuidUser, permissions[x]); if err != nil {logs.Error("VerifyPermissions error - requestType error: %s",err); return false,err}
-		if status{
-			return true,nil
-		}		
+func VerifyPermissions(uuidUser string, object string, permissions []string) (hasPermissions bool, err error) {
+	for x := range permissions {
+		status, err := UserPermissionsValidation(uuidUser, permissions[x])
+		if err != nil {
+			logs.Error("VerifyPermissions error - requestType error: %s", err)
+			return false, err
+		}
+		if status {
+			return true, nil
+		}
 	}
-	return false,nil
+	return false, nil
 }
